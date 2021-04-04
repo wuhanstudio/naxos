801341e21ee3877799be57584d65b0ab76efc05d
wuhanstudio <wuhanstudio@qq.com>
2021-04-04 15:07:10 +0100

    [add] rt-thread examples

---
+++ SConscript
@@ -0,0 +1,30 @@
+Import('RTT_ROOT')
+Import('rtconfig')
+from building import *
+
+cwd = GetCurrentDir()
+
+src = Glob('core/*.cpp')
+
+if GetDepend('NAXOS_USING_SEND_MORE_MONEY_EXAMPLE'):
+    src += Glob('examples/send_more_money.cpp')
+
+if GetDepend('NAXOS_USING_N_QUEENS_EXAMPLE'):
+    src += Glob('examples/nqueens.cpp')
+
+CPPPATH = [cwd, ]
+CPPPATH += [cwd + '/core']
+
+LOCAL_CPPFLAGS = ''
+if rtconfig.CROSS_TOOL == 'gcc':
+    LOCAL_CPPFLAGS += ' -std=c++11'
+
+group = DefineGroup('naxos', src, depend = [''], CPPPATH = CPPPATH, LOCAL_CCFLAGS=LOCAL_CPPFLAGS )
+
+Return('group')
+
+
+
+
+
+
---
+++ examples/nqueens.cpp
@@ -0,0 +1,36 @@
+// Part of https://github.com/pothitos/naxos
+
+#include <rtthread.h>
+#include <cstdlib>
+#include <iostream>
+#include <naxos.h>
+
+using namespace naxos;
+using namespace std;
+
+int nqueens(int argc, char* argv[])
+{
+        try {
+                int N = (argc > 1) ? atoi(argv[1]) : 5;
+                NsProblemManager pm;
+                NsIntVarArray Var, VarPlus, VarMinus;
+                for (int i = 0; i < N; ++i) {
+                        Var.push_back(NsIntVar(pm, 0, N - 1));
+                        VarPlus.push_back(Var[i] + i);
+                        VarMinus.push_back(Var[i] - i);
+                }
+                pm.add(NsAllDiff(Var));
+                pm.add(NsAllDiff(VarPlus));
+                pm.add(NsAllDiff(VarMinus));
+                pm.addGoal(new NsgLabeling(Var));
+                while (pm.nextSolution() != false)
+                        cout << Var << "\n";
+        } catch (exception& exc) {
+                cerr << exc.what() << "\n";
+                return 1;
+        } catch (...) {
+                cerr << "Unknown exception\n";
+                return 1;
+        }
+}
+MSH_CMD_EXPORT(nqueens, naxos: solving nqueens example)
